e4026d2a822f4bc44bbcf228ab0def820f541fc5
Sumit <168816699+SumitKilaniya@users.noreply.github.com>
2026-09-15 15:37:33 +0530

    commit modified file

--- secondl.java
+++ secondl.java
@@ -19,9 +19,3 @@ public class secondl {
         System.out.println("Second Max element is: "+secondmax);
     }
 }
-// public class  secondl{
-//     public static void main(String [] args){
-//         int [] arr= {1, 2, 4,8,1,3, 4, 5, 6, 2,5,6,7, 8, 9, 10};
-//         for (int i=0;i<ar.length )
-//     }
-// }
