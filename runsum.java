3d2ede7410d54a99c2f70d0c15f45249285e457c
Sumit <168816699+SumitKilaniya@users.noreply.github.com>
2026-09-21 15:52:07 +0530

    running sum

---
+++ runsum.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+class runsum {
+    public static void main(String[] args) {
+        int[] nums = { 1, 2, 3, 4, 45 };
+        int[] result = new int[nums.length];
+        result[0] = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            result[i] = result[i - 1] + nums[i];
+
+        }
+        System.out.println(result);
+    }
+
+}
